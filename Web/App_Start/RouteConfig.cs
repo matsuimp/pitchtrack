bfacaefa32c8966193f3ce62141a3e7a5e8f2a71
Mike Matsui <michael.matsui@gmail.com>
2013-04-13 01:56:38 -0400

    add Microsoft.AspNet.Web.Optimization

--- Web/App_Start/RouteConfig.cs
+++ Web/App_Start/RouteConfig.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace pitchtrack
@@ -16,8 +13,15 @@ namespace pitchtrack
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-			);
+				defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+				);
+		}
+	}
+
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
 		}
 	}
 }
--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using pitchtrack;
 
@@ -16,6 +17,10 @@ namespace Pitchtrack.Web
 			WebApiConfig.Register(GlobalConfiguration.Configuration);
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+			BundleTable.
 		}
+
 	}
 }
