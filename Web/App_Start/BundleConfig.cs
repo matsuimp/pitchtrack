0421cb32c642332d1f4cf2149adf2ad0e5f50763
Mike Matsui <mmatsui@groupcommerce.com>
2013-04-15 17:10:28 -0400

    getting sass and styles setup

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -11,6 +11,7 @@ namespace pitchtrack
 			bundles.Add(new ScriptBundle("~/bundles/jquery", "//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js").Include("~/Content/Scripts/jquery-{version}.js"));
 			bundles.Add(new ScriptBundle("~/bundles/handlebars", "//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.0.0-rc.3/handlebars.min.js").Include("~/Content/Scripts/handlebars.js"));
 			bundles.Add(new ScriptBundle("~/bundles/pitchtrack-utils").Include("~/Content/Scripts/PitchTrack.UriFormat.js"));
+			bundles.Add(new StyleBundle("~/Content/Styles/stylesheets").IncludeDirectory("~/Content/Styles/stylesheets", "*.css"));
 		}
 	}
 }
