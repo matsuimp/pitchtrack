06ae866ade0a9977d394e97f286f3b4380abd7b1
Jeffrey M Fairchild <jfairchild@groupcommerce.com>
2013-04-18 12:45:20 -0400

    Basic tests set up.
    using NSubstitute for mocking!

---
+++ Web.Tests/GameControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Pitchtrack.Web.Controllers;
+
+namespace Pitchtrack.Web.Tests
+{
+	[TestFixture]
+	public class GameControllerTests
+	{
+		[Test]
+		public void TestGameControllerEdit()
+		{
+			var gameController = new GameController();
+			var result = gameController.Edit();
+			Assert.IsNotNull(result);
+		}
+	}
+}
